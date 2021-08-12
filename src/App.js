182535b06c924c583f8b46d29fee83cccbc78ee8
Matrix <immatrixchan@gmail.com>
2021-08-12 10:04:48 +0800

    引入Narbar 组件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React,{Component}from 'react'
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component{
   render(){
+    const loading = true;  
+    const name = "Jon Doe";
+    
+    // if(loading){
+    //   return <h4>Loading...</h4>
+    // }
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        {/* {loading ? <h4>Loading...</h4> : <h1>Hello {name}</h1>} */}
+        <Navbar />
       </div>
     );
   }
