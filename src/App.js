dbce9e1053ecafea5634e80e2d643ec2edc112d0
Matrix <immatrixchan@gmail.com>
2021-08-12 11:05:52 +0800

    Components Props PropTypes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import './App.css';
 
 class App extends Component{
   render(){
-    const loading = true;  
-    const name = "Jon Doe";
 
     return (
       <div className="App">
-        <Navbar title="Github Find" icon="fab fa-github"/>
+        <Navbar />
       </div>
     );
   }
