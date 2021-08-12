3d1845ea720baf4a0f775d01034869a1987cc3e0
Matrix <immatrixchan@gmail.com>
2021-08-12 17:31:41 +0800

    Users 完成

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import UserItem from './UserItem';
 
 class Users extends Component {
     state = {
@@ -36,14 +37,19 @@ class Users extends Component {
 
     render() {
         return (
-            <div>
+            <div style={userStyle}>
                 {this.state.users.map(user => (
-                    <div>{user.login}</div>
-                    // <div>{}</div>
+                    <UserItem key={user.id} user={user} />
                 ))}
             </div>
-        )
+        );
     }
+}
+
+const userStyle = {
+    display:'grid',
+    gridTemplateColumns:'repeat(3, 1fr)',
+    gridGap:'1rem'
 }
 
 export default Users
