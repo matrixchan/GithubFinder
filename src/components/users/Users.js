7699ef19cab80e65ad2b2e776c16a38f35f89357
Matrix <immatrixchan@gmail.com>
2021-08-16 10:57:22 +0800

    Http Requests & Updating State

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,43 +2,43 @@ import React, { Component } from 'react'
 import UserItem from './UserItem';
 
 class Users extends Component {
-    state = {
-        users:[
-            {
-                id:'1',
-                login:'mojombo',
-                avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-                html_url: 'https://github.com/mojombo'
-            },
+    // state = {
+    //     users:[
+    //         {
+    //             id:'1',
+    //             login:'mojombo',
+    //             avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+    //             html_url: 'https://github.com/mojombo'
+    //         },
 
-            {
-                id:'2',
-                login:'defunkt',
-                avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-                html_url: 'https://github.com/defunkt'
-            },
+    //         {
+    //             id:'2',
+    //             login:'defunkt',
+    //             avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
+    //             html_url: 'https://github.com/defunkt'
+    //         },
 
-            {
-                id:'3',
-                login:'pjhyett',
-                avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-                html_url: 'https://github.com/pjhyett'
-            },
+    //         {
+    //             id:'3',
+    //             login:'pjhyett',
+    //             avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
+    //             html_url: 'https://github.com/pjhyett'
+    //         },
 
-            {
-                id:'4',
-                login:'wycats',
-                avatar_url: 'https://avatars.githubusercontent.com/u/4?v=4',
-                html_url: 'https://github.com/wycats'
-            },
+    //         {
+    //             id:'4',
+    //             login:'wycats',
+    //             avatar_url: 'https://avatars.githubusercontent.com/u/4?v=4',
+    //             html_url: 'https://github.com/wycats'
+    //         },
 
-        ]
-    }
+    //     ]
+    // }
 
     render() {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {this.props.users.map(user => (
                     <UserItem key={user.id} user={user} />
                 ))}
             </div>
