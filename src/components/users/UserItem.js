6b8bf1c683d76382139d6af296e5fe02006cf948
Matrix <immatrixchan@gmail.com>
2021-08-16 10:04:34 +0800

    Sateless Functional Components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-class UserItem extends Component {
+import PropTypes from 'prop-types'
 
+const UserItem = ({user:{login,avatar_url,html_url}})=>{
 
-    render() {
-        const {login,avatar_url,html_url} = this.props.user;
-        
-        return (
-            <div className="card text-center">
-                <img 
-                    src={avatar_url} 
-                    alt="" 
-                    className="round-img" 
-                    style={{ width:'60px'}} 
-                />
-                <h3>{login}</h3>
+    return (
+        <div className="card text-center">
+            <img 
+                src={avatar_url} 
+                alt="" 
+                className="round-img" 
+                style={{ width:'60px'}} 
+            />
+            <h3>{login}</h3>
 
-                <div>
-                    <a href={html_url} className='btn btn-dark btn-sm my-1'>
-                        More
-                    </a>
-                </div>
+            <div>
+                <a href={html_url} className='btn btn-dark btn-sm my-1'>
+                    More
+                </a>
             </div>
-        )
-    }
+        </div>
+    )
+};
+
+UserItem.propTypes={
+    user: PropTypes.object.isRequired,
 }
 
 export default UserItem
