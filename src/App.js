859241357e5d5d998109bbc7d9fb9c4495e9b362
Matrix <immatrixchan@gmail.com>
2021-09-01 12:59:58 +0800

    21 React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React,{Component}from 'react'
+import React,{Component, Fragment}from 'react'
+import { BrowserRouter as Router,Switch,  Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 import PropTypes  from 'prop-types';
 import Alert from './components/layout/Alert';
+
 
 class App extends Component{
   state = {
@@ -48,19 +51,31 @@ class App extends Component{
   render(){
     const {users, loading} = this.state;
     return (
+      <Router >
       <div className="App">
         <Navbar />
         <div className="container">
           <Alert alert={this.state.alert} />
-          <Search 
-            searchUsers = {this.searchUsers}  
-            clearUsers={this.clearUsers} 
-            showClear = {this.state.users.length > 0 ? true : false }
-            setAlert = {this.setAlert}
+          <Switch>
+            <Route 
+              exact path='/'
+              render={ props=>(
+                <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers}
+                    clearUsers={this.clearUsers}
+                    showClear={users.length > 0 ? true : false }
+                    setAlert={this.setAlert}
+                  />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )}
             />
-          <Users loading={loading} users={users}/>
+            <Route exact path="/about" component={About}/>
+          </Switch>
         </div>
       </div>
+      </Router>
     );
   }
 
