4ea5990a05a0efafa2f0e1943bb5d65111b8b890
Matrix <immatrixchan@gmail.com>
2021-09-03 17:41:32 +0800

    26 Search Class to Function With useState Hook

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,43 +1,31 @@
-import React, { Component } from 'react'
+import React,{useState} from 'react';
 import PropTypes  from 'prop-types';
 
-export class Search extends Component {
-    state = {
-        text:''
-    }
 
-    static propTypes ={
-        searchUsers: PropTypes.func.isRequired,
-        cleaarUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired,
-    }
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+    const [text, setText] = useState('');
 
-    onSubmit = (e)=>{
+    const onSubmit = (e)=>{
         e.preventDefault();
-        if(this.state.text === ''){
-            this.props.setAlert('Please enter something','light');
+        if(text === ''){
+            setAlert('Please enter something','light');
         }else{
-            this.props.searchUsers(this.state.text);
-            this.setState({ text:''});
+            searchUsers(text);
+            setText('');
         }
 
     }
 
-    onChange = (e)=>{
-        this.setState({ [e.target.name]: e.target.value });
-    }
+    const onChange = e => setText( e.target.value );
 
-    render() {
-        const {showClear,clearUsers} = this.props;
         return (
             <div>
-                <form onSubmit={this.onSubmit} className="form">
+                <form onSubmit={  onSubmit } className="form">
                     <input type="text" 
                         name="text" 
                         placeholder="Search Users..." 
-                        value={this.state.text}
-                        onChange={this.onChange}
+                        value={ text }
+                        onChange={ onChange }
                     />
                     <input 
                         type="submit" 
@@ -49,8 +37,15 @@ export class Search extends Component {
             
                 }
             </div>
-        )
-    }
+        );
+    
 }
 
-export default Search
+Search.propTypes ={
+    searchUsers: PropTypes.func.isRequired,
+    cleaarUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
+};
+
+export default Search;
